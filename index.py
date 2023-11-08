c113b9406df9c5058af8d7f4fbc777b12615d28d
sanjitcrack1234 <150055074+sanjitcrack1234@users.noreply.github.com>
2023-11-09 04:34:55 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,21 @@
 import os
 from io import BytesIO
-from queue import Queue
 import requests
 from flask import Flask, request
 from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Dispatcher
 from movies_scraper import search_movies, get_movie
 
-TOKEN = os.getenv("TOKEN")  # Use an environment variable for the bot token
+TOKEN = os.getenv("TOKEN")
 URL = "https://search-movie-bot.vercel.app/"
 bot = Bot(TOKEN)
 
+app = Flask(__name__)
 
 def welcome(update, context) -> None:
     update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
                               f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
     update.message.reply_text("👇 Enter Movie Name 👇")
-
 
 def find_movie(update, context):
     search_results = update.message.reply_text("Processing...")
@@ -31,7 +30,6 @@ def find_movie(update, context):
         search_results.edit_text('Search Results...', reply_markup=reply_markup)
     else:
         search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
-
 
 def movie_result(update, context) -> None:
     query = update.callback_query
@@ -50,23 +48,16 @@ def movie_result(update, context) -> None:
     else:
         query.message.reply_text(text=caption)
 
-
 def setup():
-    update_queue = Queue()
-    dispatcher = Dispatcher(bot, update_queue, use_context=True)
+    dispatcher = Dispatcher(bot, None, use_context=True)
     dispatcher.add_handler(CommandHandler('start', welcome))
     dispatcher.add_handler(MessageHandler(Filters.text, find_movie))
     dispatcher.add_handler(CallbackQueryHandler(movie_result))
     return dispatcher
 
-
-app = Flask(__name__)
-
-
 @app.route('/')
 def index():
     return 'Hello World!'
-
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
@@ -74,15 +65,5 @@ def respond():
     setup().process_update(update)
     return 'ok'
 
-
-@app.route('/setwebhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
-
-
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get('PORT', 5000))
+    app.run(port=int(os.environ.get('PORT', 5000)))
