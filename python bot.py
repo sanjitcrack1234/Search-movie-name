cf6a78a3e30bfe771b96135f6c05055f3de13130
sanjitcrack1234 <150055074+sanjitcrack1234@users.noreply.github.com>
2023-11-09 06:23:19 +0530

    Update python bot.py

--- python bot.py
+++ python bot.py
@@ -1,37 +1,11 @@
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from flask import Flask, request
-import requests
+from telegram import Update
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters, CallbackContext
+
+app = Flask(__name__)
 
 # Your Telegram Bot Token
 TOKEN = '6525502772:AAFQgq70G7TXJZxZsXFpv6nXMQ5RAu9TK9Q'
-WEBHOOK_URL = 'https://search-movie-name.vercel.app/setwebhook'
-
-# Create the bot instance
-bot = telegram.Bot(token=TOKEN)
-
-# Create an Updater
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-# Define your command handlers
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your movie search bot. Send me a movie name, and I'll find information about it!")
-
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-# Register command handlers
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-# Add other handlers for your bot's functionality
-
-# Start your bot
-updater.start_polling()
-
-# Create the Flask app
-app = Flask(__name)
 
 @app.route('/')
 def index():
@@ -39,16 +13,18 @@ def index():
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    url = f'https://api.telegram.org/bot{TOKEN}/setWebhook'
-    params = {
-        'url': WEBHOOK_URL + '/' + TOKEN
-    }
-
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
+    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
         return "Webhook setup ok"
     else:
         return "Webhook setup failed"
 
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def webhook():
+    update = Update.de_json(request.get_json(), bot)
+    dispatcher.process_update(update)
+    return ''
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Start the Flask app
+    app.run(port=5000)
