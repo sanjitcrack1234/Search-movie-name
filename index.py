07d9b5e276d2805bdf815a4fec0d247eac24a83d
sanjitcrack1234 <150055074+sanjitcrack1234@users.noreply.github.com>
2023-11-09 06:33:11 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,64 +6,10 @@ from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Dispatcher
 from movies_scraper import search_movies, get_movie
 
-TOKEN = "6525502772:AAFQgq70G7TXJZxZsXFpv6nXMQ5RAu9TK9Q"
-URL = "https://search-movie-name.vercel.app/"
+TOKEN = "Your_Telegram_Bot_Token"
+URL = "https://your-vercel-app.vercel.app/"  # Update this with your Vercel app URL
 bot = Bot(TOKEN)
 
 app = Flask(__name__)
 
-def welcome(update, context) -> None:
-    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
-                              f"🔥 Download Your Favorite Movies For 💯 Free And 🍿 Enjoy it.")
-    update.message.reply_text("👇 Enter Movie Name 👇")
-
-def find_movie(update, context):
-    search_results = update.message.reply_text("Processing...")
-    query = update.message.text
-    movies_list = search_movies(query)
-    if movies_list:
-        keyboards = []
-        for movie in movies_list:
-            keyboard = InlineKeyboardButton(movie["title"], callback_data=movie["id"])
-            keyboards.append([keyboard])
-        reply_markup = InlineKeyboardMarkup(keyboards)
-        search_results.edit_text('Search Results...', reply_markup=reply_markup)
-    else:
-        search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
-
-def movie_result(update, context) -> None:
-    query = update.callback_query
-    s = get_movie(query.data)
-    response = requests.get(s["img"])
-    img = BytesIO(response.content)
-    query.message.reply_photo(photo=img, caption=f"🎥 {s['title']}")
-    link = ""
-    links = s["links"]
-    for i in links:
-        link += "🎬" + i + "\n" + links[i] + "\n\n"
-    caption = f"⚡ Fast Download Links :-\n\n{link}"
-    if len(caption) > 4095:
-        for x in range(0, len(caption), 4095):
-            query.message.reply_text(text=caption[x:x+4095])
-    else:
-        query.message.reply_text(text=caption)
-
-def setup():
-    dispatcher = Dispatcher(bot, None, use_context=True)
-    dispatcher.add_handler(CommandHandler('start', welcome))
-    dispatcher.add_handler(MessageHandler(Filters.text, find_movie))
-    dispatcher.add_handler(CallbackQueryHandler(movie_result))
-    return dispatcher
-
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond():
-    update = Update.de_json(request.get_json(force=True), bot)
-    setup().process_update(update)
-    return 'ok'
-
-if __name__ == "__main__":
-    app.run(port=int(os.environ.get('PORT', 5000)))
+# ... (rest of the code remains the same)
